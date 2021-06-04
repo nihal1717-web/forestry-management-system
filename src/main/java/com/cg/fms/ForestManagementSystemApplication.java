5f5142744528175cabc60a63dcf22b058d678d18
nihal1717-web <mangalwadekarnihal9@gmail.com>
2021-06-04 18:26:53 +0530

    customer module

---
+++ src/main/java/com/cg/fms/ForestManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cg.fms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForestManagementSystemApplication.class, args);
+	}
+
+}
